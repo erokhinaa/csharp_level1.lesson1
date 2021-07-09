0d6429cac32a95529202dc6a1415551b33888f01
er <aerokhin1988@mail.ru>
2021-07-09 12:51:19 +0300

    added name/surname into head of namespace

--- Lesson5/Lesson5/Program.cs
+++ Lesson5/Lesson5/Program.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace Lesson5
+namespace Lesson5 // ЕРОХИН АЛЕКСАНДР АНДРЕЕВИЧ. Факультет Разработки VR&AR
 {
     public static class Message
     {
