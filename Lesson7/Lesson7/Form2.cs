3cd6b1b1db7a19e0e9da880d5417b26c702ba0fa
er <aerokhin1988@mail.ru>
2021-07-13 20:52:08 +0300

    added lesson7

---
+++ Lesson7/Lesson7/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson7
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNum_Click(object sender, EventArgs e)
+        {
+            int num;
+            int r;
+            Random rnd = new Random();
+            r = rnd.Next(1,100);
+            num = int.Parse(boxNum.Text);
+
+            if (num > r) MessageBox.Show("Ваше число больше загаданного");
+            else if (num < r) MessageBox.Show("Ваше число меньше загаданного");
+            else MessageBox.Show("Поздравляю! Вы угадали");
+        }
+    }
+}
---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson7
+{
+    static class Program // ЕРОХИН АЛЕКСАНДР АНДРЕЕВИЧ. Факультет Разработки VR&AR
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            #region 1.
+            // а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
+            // б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок. Игрок должен получить это число за минимальное количество ходов.
+            // в) *Добавить кнопку «Отменить», которая отменяет последние ходы. Используйте библиотечный обобщенный класс System.Collections.Generic.Stack<int> Stack.
+            // Вся логика игры должна быть реализована в классе с удвоителем.
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            #endregion
+
+            #region 2. Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток. Компьютер говорит, больше или меньше загаданное число введенного.
+            // a) Для ввода данных от человека используется элемент TextBox;
+            // б) **Реализовать отдельную форму c TextBox для ввода числа.
+            Application.Run(new Form2());
+            #endregion
+        }
+    }
+}
